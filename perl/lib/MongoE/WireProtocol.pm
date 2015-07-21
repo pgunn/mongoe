ff533c46f52a08d4af785124aab261a21c44ac89
Pat Gunn <pgunn@mongodb.com>
2015-07-21 13:12:10 -0400

    Stubwork for pulling Wire Protocol fns into library

---
+++ perl/lib/MongoE/WireProtocol.pm
@@ -0,0 +1,16 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+package MongoE::WireProtocol;
+
+require Exporter;
+require AutoLoader;
+
+our @ISA = qw(Exporter AutoLoader);
+our @EXPORT_OK = qw(); # Functions exposed to clients
+our @EXPORT = @EXPORT_OK;
+
+
+
+1;
