1ea8849bce91ba27f7b187675091d8b917c7571e
Pat Gunn <pgunn@mongodb.com>
2015-07-21 13:29:35 -0400

    pod template for the library

--- perl/lib/MongoE/WireProtocol.pm
+++ perl/lib/MongoE/WireProtocol.pm
@@ -11,6 +11,43 @@ our @ISA = qw(Exporter AutoLoader);
 our @EXPORT_OK = qw(); # Functions exposed to clients
 our @EXPORT = @EXPORT_OK;
 
+=pod
+
+=head1 NAME
+
+MongoE::WireProtocol - WireProtocol components of MongoE
+
+=head1 SYNOPSIS
+
+Use this to decode and encode information between the MongoDB Wire Protocol and native formats.
+
+=head1 METHODS
+
+=cut
+
+=pod
+
+Document method here
+
+=cut
 
 
 1;
+
+__END__
+
+=pod
+
+=head1 TO DO
+
+Many improvements
+
+=head1 BUGS
+
+Probably Many
+
+=head1 AUTHORS
+
+Pat Gunn <pgunn@dachte.org>
+
+=cut
